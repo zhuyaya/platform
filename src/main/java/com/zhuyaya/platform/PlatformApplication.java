9aa4073403206653a5fc468179fb247005928067
zhuyaya <15601937556@163.com>
2021-12-18 23:58:40 +0800

    项目初启动

--- src/main/java/com/zhuyaya/platform/PlatformApplication.java
+++ src/main/java/com/zhuyaya/platform/PlatformApplication.java
@@ -3,6 +3,9 @@ package com.zhuyaya.platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhuyaya
+ */
 @SpringBootApplication
 public class PlatformApplication {
 
